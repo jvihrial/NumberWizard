2dcb223ad55897694519fb1d99fb365d2693f5d5
Jarko Vihriala <jarko@unity3d.com>
2015-09-14 21:23:13 +0300

    Lecture 16 completed.

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -7,6 +7,13 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me.");
+		
+		int max = 1000;
+		int min = 1;	
+		
+		print ("Highest number you can enter is " + max );
+		print ("..and lowest nuber is " + min );
+		
 	}
 	
 	// Update is called once per frame
