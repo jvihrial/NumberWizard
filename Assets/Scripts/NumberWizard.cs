51cf87be0299ece2278ec512c5fcd559f26b25a5
Jarko Vihriala <jarko@unity3d.com>
2015-09-14 22:06:09 +0300

    Lecture 18 complete

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -14,10 +14,21 @@ public class NumberWizard : MonoBehaviour {
 		print ("Highest number you can enter is " + max );
 		print ("..and lowest nuber is " + min );
 		
+		print ("Is your number greater or lower than 500 ?");
+		print ("Press up arrow for higher, down for lower and enter for equal");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+			
+		if (Input.GetKeyDown( KeyCode.UpArrow)) {
+			print ("Up arrow pressed");
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			print ("Down arrow pressed");
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("Enter key pressed");
+		}
 		
-	}
+	}		
 }
